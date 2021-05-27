93e7bdff44dd2477e71acdcaa7152c2467228ea2
851605063@qq.com <851605063@qq.com>
2021-05-27 21:25:09 +0800

    20210527行内

--- src/main/java/com/qfggk/simpleweb/controller/HelloController.java
+++ src/main/java/com/qfggk/simpleweb/controller/HelloController.java
@@ -1,16 +1,35 @@
 package com.qfggk.simpleweb.controller;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 @Controller
 @ResponseBody
+@Slf4j
 public class HelloController {
 
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
     @GetMapping("/hello")
-    public String hello()
+    @ResponseBody
+    public String hello(@RequestParam("id") String id)
     {
-        return "hello world";
+        String sql="select * from bms_books where book_id="+id;
+
+        List<Map<String, Object>> maps = jdbcTemplate.queryForList(sql);
+        log.info("查询记录:{}", maps.get(0).toString());
+        return maps.get(0).toString();
     }
 }
