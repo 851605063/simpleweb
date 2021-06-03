0e1ab56178e0b68266ebcff0028ce9400d1cebc1
851605063@qq.com <851605063@qq.com>
2021-06-03 22:08:51 +0800

    20210603行内

--- src/main/java/com/qfggk/simpleweb/controller/TableController.java
+++ src/main/java/com/qfggk/simpleweb/controller/TableController.java
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -30,6 +32,16 @@ public class TableController {
     public String responsive_table()
     {
         return "table/responsive_table";
+    }
+
+    @GetMapping("/user/delete/{id}")
+    public String deleteuser(@PathVariable("id") Integer id,
+                             @RequestParam("pn") Integer pn,
+                             RedirectAttributes ra)
+    {
+        ra.addAttribute("pn",pn);
+        userService.removeById(id);
+        return "redirect:/dynamic_table";
     }
 
     @GetMapping("/dynamic_table")
